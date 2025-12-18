a5d14d18db5af9a1486b1323c4a5f22b3c433a1a
DarkDobby <dodygamepad2@gmail.com>
2025-12-18 22:10:16 +0200

    feat(frontend): add navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {HashLink} from "react-router-hash-link";
+
+function Navbar() {
+    return (
+        <nav className='bg-white shadow-md text-[#008e9b] flex justify-between'>
+            <div>
+                <img className='w-16 ml-20' src='/logo.png' alt='logo' />
+            </div>
+
+            <ul className='flex space-x-6 items-center px-4 mr-20'>
+                <li><Link to="/">Home</Link></li>
+                <li><HashLink smooth to="/#about">About</HashLink></li>
+
+                {user?.role === "admin" &&
+                    <>
+                        <li><Link to="/add-doctor">Add Doctor</Link></li>
+                        <li><Link to="/add-department">Add Department</Link></li>
+                    </>}
+
+                {user?.role === "user" &&
+                    <>
+                        <li><Link to="/add-appointment">Add Appointment</Link></li>
+                        <li><Link to="/my-appointments">My Appointments</Link></li>
+                    </>}
+
+                {!user &&
+                    <>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
+                    </>}
+
+                {user &&
+                    <li>
+                        <button onClick={logout} className='rounded hover:bg-red-500 text-white py-1'>logout</button>
+                    </li>}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
