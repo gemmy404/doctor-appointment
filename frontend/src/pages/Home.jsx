609c125bc55456240aac5dc59ab381c2dcdf48e1
Nadia <nadiaashraff220@gmail.com>
2025-12-19 13:22:40 +0200

    feat(frontend): add stats component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import HeroSlider from "../components/HeroSlider";
 import CallToAction from "../components/CallToAction";
 import About from "../components/About";
+import Stats from "../components/Stats";
 
 function Home() {
     return (
         <div>
-            <HeroSlider/>
-            <CallToAction/>
-            <About/>
+            <HeroSlider />
+            <CallToAction />
+            <About />
+            <Stats />
         </div>
     )
 }
